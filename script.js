30b0d58ae536f79c062c50929629377611c3644e
Lesmana Zimmer <lesmana@gmx.de>
2019-01-04 07:57:49 +0100

    initiate and respond

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function clickinitiate() {
+  console.log('clickinitiate');
+  document.getElementById('spanbuttons').classList.toggle('invisible');
+  document.getElementById('spaninitiate').classList.toggle('invisible');
+}
+
+function clickrespond() {
+  console.log('clickrespond');
+  document.getElementById('spanbuttons').classList.toggle('invisible');
+  document.getElementById('spanrespond').classList.toggle('invisible');
+}
